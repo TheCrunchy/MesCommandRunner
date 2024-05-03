447a00c45c201eafe9c54f646f854fed6157f89b
TheCrunchy <crunchycamsta@gmail.com>
2024-05-03 18:54:15 +0100

    Add project files.

---
+++ MesCommandRunner/Core.cs
@@ -0,0 +1,60 @@
+﻿using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.GameSystems;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MesCommandRunner.Commands;
+using Torch;
+using Torch.API;
+using Torch.API.Managers;
+using Torch.API.Plugins;
+using Torch.API.Session;
+using Torch.Managers;
+using Torch.Managers.PatchManager;
+using Torch.Session;
+
+namespace MesCommandRunner
+{
+    public class Core : TorchPluginBase
+    {
+        public TorchSessionState TorchState;
+        public static MESApi MesAPI;
+        public override void Init(ITorchBase torch)
+        {
+            base.Init(torch);
+
+            var sessionManager = Torch.Managers.GetManager<TorchSessionManager>();
+
+            if (sessionManager != null)
+            {
+                sessionManager.SessionStateChanged += SessionChanged;
+            }
+
+            SetupConfig();
+
+        }
+
+        public override void Update()
+        {
+        }
+
+        private void SetupConfig()
+        {
+          //no config file but me lazy so keeping it here if i need one
+        }
+
+        private void SessionChanged(ITorchSession session, TorchSessionState newState)
+        {
+            TorchState = newState;
+            if (newState is TorchSessionState.Loaded)
+            {
+                //example stuff
+            }
+        }
+    }
+}
+
---
+++ MesCommandRunner/Patches/SessionLoadPatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MesCommandRunner.Commands;
+using Sandbox.Game.SessionComponents;
+using Torch.Managers.PatchManager;
+using VRage.Game.Components;
+
+namespace MesCommandRunner.Patches
+{
+  [PatchShim]
+    public static class SessionLoadPatch
+    {
+        public static void Patch(PatchContext ctx)
+        {
+            ctx.GetPattern(contract).Prefixes.Add(contractPatch);
+        }
+        internal static readonly MethodInfo contract =
+            typeof(MySessionComponentBase).GetMethod("LoadData",
+                BindingFlags.Instance | BindingFlags.Public) ??
+            throw new Exception("Failed to find patch method contract");
+
+        internal static readonly MethodInfo contractPatch =
+            typeof(SessionLoadPatch).GetMethod(nameof(LoadData), BindingFlags.Static | BindingFlags.Public) ??
+            throw new Exception("Failed to find patch method");
+
+        public static bool Loaded = false;
+
+        public static void LoadData()
+        {
+            if (!Loaded)
+            {
+                Core.MesAPI = new MESApi();
+
+               Loaded = true;
+            }
+        }
+
+    }
+}
